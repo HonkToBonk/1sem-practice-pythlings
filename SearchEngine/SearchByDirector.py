366722d63c99b155463202e71f41ed29f013d339
lvv19u525 <vladleonov21032002@gmail.com>
2020-06-07 16:33:02 +0300

    SearchByDirector was integrated

--- SearchEngine/SearchByDirector.py
+++ SearchEngine/SearchByDirector.py
@@ -5,9 +5,9 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 import time
-from SearchByName import *
+from .SearchByName import *
 
-def by_director(in_director):
+def by_director(in_director, msg,bot):
     chrome_options = Options()
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
@@ -20,23 +20,22 @@ def by_director(in_director):
     elem.send_keys(in_director)
     elem.send_keys(Keys.RETURN) 
     for i in range(1,0, -1):
-                time.sleep(1)
+        time.sleep(1)
     try:
         driver.find_element_by_partial_link_text(in_director).click()
         for i in range(1,0, -1):
                     time.sleep(0.5)
         name = driver.find_element_by_tag_name("h1").text
-        print(name)
         get_stars = driver.find_elements_by_tag_name("li")
         film1 = get_stars[7].text
         film2 = get_stars[8].text
         film3 = get_stars[9].text
         driver.close()
-        by_name(film1)
-        by_name(film2)
-        by_name(film3)
-    except NoSuchElementException:
+        by_name(film1,msg,bot)
+        by_name(film2,msg,bot)
+        by_name(film3,msg,bot)
+    except:
         driver.close()
-        print("Sorry, can't find that film.")
+        return '-1'
 
 
--- source.py
+++ source.py
@@ -4,6 +4,7 @@ from telebot import types
 from mongoengine import *
 from SearchEngine.SearchByName import *
 from SearchEngine.SearchByYear import *
+from SearchEngine.SearchByDirector import *
 
 
 bot = telebot.TeleBot('1002991204:AAHnJ2q9kV1htX5iRREZpW0Vg_6xOFOOSao')
@@ -168,7 +169,12 @@ def search_actor(msg):
 @bot.message_handler(func=lambda msg: (State.objects(user_id=str(msg.chat.id)))[0].state[-1] == 'режиссер' and \
                                       (State.objects(user_id=str(msg.chat.id)))[0].mode == 'фильм')
 def search_director(msg):
-    pass
+    if by_director(msg.text, msg, bot) == '-1':
+        bot.send_message(msg.chat.id, text='К сожалению мне не удалось ничего найти')
+    user_data = (State.objects(user_id=str(msg.chat.id)))[0]
+    user_data.state.append('Нашел')
+    user_data.save()
+    bot.send_message(msg.chat.id, text='Введите команду /restart, чтобы подобрать что-нибудь новенькое.')
 
 
 @bot.message_handler(func=lambda msg: (State.objects(user_id=str(msg.chat.id)))[0].state[-1] == 'жанр' and \
